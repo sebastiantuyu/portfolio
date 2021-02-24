dd8b84b38fe55e38ffc58591f9873b26f1e13c3a
sebastiantuyu <alextpineiro@gmail.com>
2021-02-24 10:56:04 -0600

    Adding changes on ttitle

--- assets/js.js
+++ assets/js.js
@@ -1,11 +1,19 @@
-/*let x = document.getElementById('img-container')
+let mob = false
+let x = document.getElementById('img-container')
+if (window.innerWidth<1024){
+  x = document.getElementById('img-container-mobile')
+  mob = true
+}else{
+  //let x = document.getElementById('img-container')
+}
+//let x = document.getElementById('img-container')
+
 window.addEventListener('scroll',function(e){
   if(this.scrollY>=this.innerHeight){
-  setTimeout(function(){
-x.scroll({
-    top: 500,
-    behavior: "smooth"
-  });
-  },1000)
-}  
-})*/
+        setTimeout(function(){
+          if(mob){x.scroll({left: 200,behavior: "smooth"});}
+          else{x.scroll({top: 500,behavior: "smooth"});}
+
+                },1000)
+        }  
+        })
